ca2ec26a3276fcb2245afda46a55040822f88410
Jack1e <djack1e2401@gmail.com>
2025-11-06 14:29:02 +0700

    dashboard/page without feature

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace HosipitalManager
+﻿using HospitalManager.MVVM.Views;
+
+namespace HosipitalManager
 {
     public partial class App : Application
     {
@@ -6,7 +8,7 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new DashboardPageView();
         }
     }
 }
