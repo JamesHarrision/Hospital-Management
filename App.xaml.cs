bb8b1a19b83023eeaecab18f30d1a3e879fad178
Jack1e <djack1e2401@gmail.com>
2025-11-06 13:36:28 +0700

    init project

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace HosipitalManager
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HosipitalManager
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
